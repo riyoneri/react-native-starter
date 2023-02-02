4d910a8a38086a80ad1741de6588e7f0b674108c
riyoneri <lionkaneza@gmail.com>
2023-02-02 13:39:40 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is React Native starter by Riyoneri</Text>
       <StatusBar style="auto" />
     </View>
   );
